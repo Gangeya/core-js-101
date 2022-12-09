71be2c3a21dd7ee8fa35db766479cbcb60cb194a
Eugene Mikhalionok <eugenemikhalionok@gmail.com>
2022-12-09 21:48:36 +0300

    feat: implement function getElementByIndexes

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -581,8 +581,9 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  const result = indexes.reduce((acc, curr) => acc[curr], arr);
+  return result;
 }
 
 /**
