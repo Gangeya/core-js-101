93ba391f2823d32903cd149be7d7d9cc0a86747f
Eugene Mikhalionok <eugenemikhalionok@gmail.com>
2022-12-09 22:19:44 +0300

    feat: implement swapHeadAndTail function

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -604,8 +604,20 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const half = Math.floor(arr.length / 2);
+  const head = arr.slice(0, half);
+  const tail = arr.slice(-half);
+  let result = null;
+  if (arr.length % 2 === 0) {
+    result = [...tail, ...head];
+  } else if (arr.length === 1) {
+    result = arr;
+  } else {
+    const middlEl = arr[half];
+    result = [...tail, middlEl, ...head];
+  }
+  return result;
 }
 
 module.exports = {
