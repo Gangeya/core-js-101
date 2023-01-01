1fecb5de6a9c1a32549a8ea7c9bcfd9a434f5069
Eugene Mikhalionok <eugenemikhalionok@gmail.com>
2023-01-01 23:40:07 +0300

    feat: implement getDigitalRoot

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -296,8 +296,11 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  if (num > 9) {
+    return getDigitalRoot([...`${num}`].reduce((sum, cur) => +sum + +cur, 0));
+  }
+  return num;
 }
 
 /**
